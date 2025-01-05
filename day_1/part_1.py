b682e2ff50c6753db2396446b8181f4c1e2169d1
William May <wjrm500@gmail.com>
2025-01-05 10:22:23 +0000

    Day 1 - Part 2

--- day_1/part_1.py
+++ day_1/part_1.py
@@ -6,25 +6,17 @@ with open(INPUT_FILE) as f:
     lines = f.readlines()
 
 calibration_value_sum = 0
-
 for line in lines:
     first_val, second_val = None, None
-
-    # Get first value
     for letter in line:
         if letter.isdigit():
             first_val = letter
             break
-    
-    # Get second value
     for letter in reversed(line):
         if letter.isdigit():
             second_val = letter
             break
-    
-    # Calculate calibration value
     calibration_value = int(first_val + second_val)
-    
     calibration_value_sum += calibration_value
 
 print(calibration_value_sum)
---
+++ day_1/part_2.py
@@ -0,0 +1,35 @@
+import os
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), "input_test.txt")
+
+with open(INPUT_FILE) as f:
+    lines = f.readlines()
+lines = [line.rstrip("\n") for line in lines]
+
+number_word_to_digit_mapping = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+number_words_and_digits = list(number_word_to_digit_mapping.keys()) + list(number_word_to_digit_mapping.values())
+calibration_value_sum = 0
+for line in lines:
+    first_found = min(
+        (sub for sub in number_words_and_digits if sub in line),
+        key=line.index,
+    )
+    first_found = number_word_to_digit_mapping.get(first_found, first_found)
+    last_found = max(
+        (sub for sub in number_words_and_digits if sub in line),
+        key=line.rindex,
+    )
+    last_found = number_word_to_digit_mapping.get(last_found, last_found)
+    calibration_value_sum += int(first_found + last_found)
+print(calibration_value_sum)
+# Answer: 54,078 - Correct
