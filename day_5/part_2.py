520566d6eb8447643daad5ce9cb7e6e09677f762
William May <wjrm500@gmail.com>
2025-01-07 00:46:52 +0000

    Day 5 - Part 2 (unfinished)

---
+++ day_5/part_2.py
@@ -0,0 +1,136 @@
+import math
+import os
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Any
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), "input_example_1.txt")
+
+with open(INPUT_FILE) as f:
+    text = f.read()
+
+
+@dataclass
+class RangeMap:
+    source_range_start: int
+    dest_range_start: int
+    range_length: int
+
+
+@dataclass
+class Modulator:
+    source_range_start: int
+    source_range_end: int
+    modulate_by: int
+
+
+@dataclass
+class CategoryMap:
+    range_maps: list[RangeMap]
+    _modulators: list[Modulator] | None = field(default=None, init=False)
+
+    @property
+    def modulators(self) -> list[Modulator]:
+        if not self._modulators:
+            gappy_modulators: list[Modulator] = []
+            for range_map in self.range_maps:
+                source_range_end = range_map.source_range_start + range_map.range_length
+                modulate_by = range_map.dest_range_start - range_map.source_range_start
+                gappy_modulators.append(
+                    Modulator(
+                        source_range_start=range_map.source_range_start,
+                        source_range_end=source_range_end,
+                        modulate_by=modulate_by,
+                    )
+                )
+            gappy_modulators = sorted(gappy_modulators, key=lambda m: m.source_range_start)
+            gap_free_modulators: list[Modulator] = []
+
+            # Fill in gaps between modulators
+            for i in range(len(gappy_modulators) - 1):
+                if i == 0:
+                    gap_free_modulators.append(gappy_modulators[i])
+                if gappy_modulators[i + 1].source_range_start > gappy_modulators[i].source_range_end:
+                    new_modulator = Modulator(
+                        source_range_start=gappy_modulators[i].source_range_end,
+                        source_range_end=gappy_modulators[i + 1].source_range_start,
+                        modulate_by=0,
+                    )
+                    gap_free_modulators.append(new_modulator)
+                gap_free_modulators.append(gappy_modulators[i + 1])
+            
+            # Add initial modulator starting at 0
+            if gap_free_modulators[0].source_range_start > 0:
+                initial_modulator = Modulator(
+                    source_range_start=0,
+                    source_range_end=gap_free_modulators[0].source_range_start,
+                    modulate_by=0
+                )
+                gap_free_modulators.insert(0, initial_modulator)
+            
+            # Add final modulator ending at infinity
+            final_modulator = Modulator(
+                source_range_start=gap_free_modulators[-1].source_range_end,
+                source_range_end=math.inf,
+                modulate_by=0,
+            )
+            gap_free_modulators.append(final_modulator)
+
+            self._modulators = gap_free_modulators
+        return self._modulators
+    
+    def modulate_ranges_by(self, ranges_modulated_by: list[tuple[range, int]]) -> list[tuple[range, int]]:
+        new_ranges_modulated_by = []
+        for range_, modulated_by in ranges_modulated_by:
+            for modulator in category_map.modulators:
+                range_overlap = range(
+                    max(range_.start, modulator.source_range_start),
+                    min(range_.stop, modulator.source_range_end),
+                )
+                if range_overlap.stop > range_overlap.start:
+                    new_ranges_modulated_by.append((range_overlap, modulated_by + modulator.modulate_by))
+        return new_ranges_modulated_by
+
+
+text_sections = text.split("\n\n")
+
+maps_text_sections = text_sections[1:]
+category_maps: list[CategoryMap] = []
+for map_text_section in maps_text_sections:
+    range_maps_text = map_text_section.split(":")[1]
+    range_maps = []
+    for range_map_text in range_maps_text.strip("\n").split("\n"):
+        dest_range_start_text, source_range_start_text, range_length_text = range_map_text.split()
+        range_map = RangeMap(
+            source_range_start=int(source_range_start_text),
+            dest_range_start=int(dest_range_start_text),
+            range_length=int(range_length_text),
+        )
+        range_maps.append(range_map)
+    category_map = CategoryMap(range_maps=range_maps)
+    category_maps.append(category_map)
+
+def chunk_list(l: list[Any], n: int) -> list[list[Any]]:
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+
+seeds_text_section = text_sections[0]
+seed_numbers = list(map(int, seeds_text_section.split(": ")[1].split()))
+seed_range_chunks = list(chunk_list(seed_numbers, 2))
+seed_ranges: list[range] = []
+for seed_range_chunk in seed_range_chunks:
+    seed_range = range(
+        seed_range_chunk[0],
+        seed_range_chunk[0] + seed_range_chunk[1],
+    )
+    seed_ranges.append(seed_range)
+
+ranges_modulated_by: list[tuple[range, int]] = [(seed_range, 0) for seed_range in seed_ranges]
+for category_map in category_maps:
+    ranges_modulated_by = category_map.modulate_ranges_by(ranges_modulated_by)
+
+lowest_location_number_overall = math.inf
+for range_, modulated_by in ranges_modulated_by:
+    lowest_location_number_in_range = range_.start + modulated_by
+    lowest_location_number_overall = min(lowest_location_number_overall, lowest_location_number_in_range)
+print(lowest_location_number_overall)
