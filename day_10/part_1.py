a32035be9189f3723d54bcfa7df24f014fb9d1da
William May <wjrm500@gmail.com>
2025-01-12 00:14:49 +0000

    Day 10 - Part 1 (rewritten completely and now working)

--- day_10/part_1.py
+++ day_10/part_1.py
@@ -1,58 +1,90 @@
 import os
 
-INPUT_FILE = os.path.join(os.path.dirname(__file__), "input_example_1.txt")
+INPUT_FILE = os.path.join(os.path.dirname(__file__), "input_test.txt")
 
 with open(INPUT_FILE) as f:
     input_text = f.read()
 
-translocate = {
-   (-1, 0): { # North
-      "|": (-1, 0),
-      "F": (-1, 1),
-      "7": (-1, -1),
-   },
-   (0, 1): { # East
-      "-": (0, 1),
-      "J": (-1, 1),
-      "7": (1, 1),
-   },
-   (1, 0): { # South
-      "|": (1, 0),
-      "L": (1, 1),
-      "J": (1, -1),
-   },
-   (0, -1): { # West
-      "-": (0, -1),
-      "L": (-1, -1),
-      "F": (1, -1),
-   },
-}
+char_corner_mapping = {"F": "⌜", "7": "⌝", "J": "⌟", "L": "⌞"}
 lines = input_text.split("\n")
-matrix = [[c for c in line] for line in lines]
+matrix = [[char_corner_mapping.get(c, c) for c in line] for line in lines]
 
-def find_start_position():
-   for i in range(len(matrix)):
-      line = matrix[i]
-      for j in range(len(line)):
-         point = matrix[i][j]
-         if point == "S":
-            return i, j
-   raise Exception("No starting point found")
+def find_start_position() -> tuple[int, int]:
+    for i in range(len(matrix)):
+        line = matrix[i]
+        for j in range(len(line)):
+            point = matrix[i][j]
+            if point == "S":
+                return i, j
+    raise Exception("No starting point found")
 
-current_position = find_start_position()
-travelled = 0
-while True:
-   checks = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-   for check in checks:
-      try:
-         next_pipe = matrix[current_position[0] + check[0]][current_position[1] + check[1]]
-      except IndexError:
-         continue
-      if (shift := translocate[check].get(next_pipe)):
-         travelled += 1 if next_pipe in ("-", "|") else 2
-         current_position = current_position[0] + shift[0], current_position[1] + shift[1]
-         break
-   current_pipe = matrix[current_position[0]][current_position[1]]
-   if current_pipe == "S":
-      break
-print(travelled / 2)
+direction_shift_mapping = {
+    "N": (-1, 0),
+    "E": (0, 1),
+    "S": (1, 0),
+    "W": (0, -1),
+}
+
+def complete_loop(position: tuple[int, int], direction: str) -> int:
+    steps = 0
+    while True:
+        shift = direction_shift_mapping[direction]
+        next_pipe = matrix[position[0] + shift[0]][position[1] + shift[1]]
+        position = (position[0] + shift[0], position[1] + shift[1])
+        steps += 1
+        if next_pipe == "S":
+            return steps
+        if direction == "N":
+            if next_pipe == "|":
+                continue
+            elif next_pipe == "⌜":
+                direction = "E"
+                continue
+            elif next_pipe == "⌝":
+                direction = "W"
+                continue
+            else:
+                raise Exception("Loop failed")
+        elif direction == "E":
+            if next_pipe == "-":
+                continue
+            elif next_pipe == "⌟":
+                direction = "N"
+                continue
+            elif next_pipe == "⌝":
+                direction = "S"
+                continue
+            else:
+                raise Exception("Loop failed")
+        elif direction == "S":
+            if next_pipe == "|":
+                continue
+            elif next_pipe == "⌞":
+                direction = "E"
+                continue
+            elif next_pipe == "⌟":
+                direction = "W"
+                continue
+            else:
+                raise Exception("Loop failed")
+        elif direction == "W":
+            if next_pipe == "-":
+                continue
+            elif next_pipe == "⌞":
+                direction = "N"
+                continue
+            elif next_pipe == "⌜":
+                direction = "S"
+                continue
+            else:
+                raise Exception("Loop failed")
+
+start_position = find_start_position()
+for direction in ["N", "E", "S", "W"]:
+    try:
+        steps = complete_loop(start_position, direction)
+        break
+    except:
+        continue
+print(int(steps / 2))
+# Correct
